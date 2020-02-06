324561b4ebeea3f92a01521bf47eded0aafc6b10
Gerardo Ayala Juarez <gera.ay.ju@gmail.com>
2020-02-06 09:57:56 -0600

    uwu

--- Proyecto_Unidad_1/src/lectorio/LectorArchivo.java
+++ Proyecto_Unidad_1/src/lectorio/LectorArchivo.java
@@ -13,13 +13,29 @@ public class LectorArchivo{
 
     public LectorArchivo(){
         try {
-            archivo = "datos.txt";d
+            archivo = "datos.txt";
             fis = new FileInputStream(archivo);
             canal = fis.getChannel();
-            buffer = ByteBuffer.allocateDirect(1024);
+            buffer = ByteBuffer.allocate(1024);
+            int bytesleidos = canal.read(buffer);
+            while(bytesleidos!=-1){
+                System.out.println("Bytes leidos"+bytesleidos);
+                buffer.flip();
+                    while(buffer.hasRemaining()){
+                        System.out.print((char)buffer.get());
+                    }
+                    buffer.clear();
+                    bytesleidos = canal.read(buffer);
+            }
+            System.out.println();
+            fis.close();
 
         } catch (Exception e) {
             //TODO: handle exception
         }
+    }
+    public static void main(String[] args) {
+        LectorArchivo hola = new LectorArchivo();
+
     }
 }
